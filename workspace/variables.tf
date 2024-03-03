208cc4c2cf62e2966951b853eb2453a810276d5c
vsathish2012 <vsathish.devops@gmail.com>
2024-03-03 17:10:44 +0530

    new files added

---
+++ workspace/variables.tf
@@ -0,0 +1,28 @@
+variable cidr_block {
+}
+variable tag {
+}
+variable subnet1_cidr_block {
+}
+variable availability_zone {
+}
+variable subnet2_cidr_block {
+}
+
+variable terraform-key-pair {
+}
+
+variable ami {
+}
+
+variable instance_type {
+}
+
+variable availability_zone_1 {
+}
+
+variable autoscaling_group_min_size {
+}
+
+variable autoscaling_group_max_size {
+}
